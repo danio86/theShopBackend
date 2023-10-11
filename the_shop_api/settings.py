3ddd1ba28034eb5e064041537c89c711dd7bb390
danio86 <d.harder@gmx.at>
2023-10-11 06:22:17 +0000

    add profile sterializer and connected with profiles

--- the_shop_api/settings.py
+++ the_shop_api/settings.py
@@ -39,7 +39,7 @@ SECRET_KEY = 'django-insecure-ja=c@jfw0@r5^$_cb01nh(k+jd@b*afd)b6b$coza)8^l6&)ec
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['yourdomain.com', '8000-danio86-theshopbackend-gj1p42gjdh1.ws-eu105.gitpod.io']
+ALLOWED_HOSTS = ['yourdomain.com', '8000-danio86-theshopbackend-yar9uu6pudi.ws-eu105.gitpod.io']
 
 
 # Application definition
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
+    'rest_framework',
 
      'profiles',
 ]
