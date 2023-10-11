67f14ce506f7a0f1686e75b95cb19a34ff0c3375
danio86 <d.harder@gmx.at>
2023-10-11 18:04:49 +0000

    add properties model and sterializer

---
+++ properties/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Image(models.Model):
+    property = models.ForeignKey(Property, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='property_images/')
+
+class Property(models.Model):
+    """
+    Property model, related to 'owner', i.e. a User instance.
+    Default image set so that we can always reference image.url.
+    """
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    size = models.DecimalField(max_digits=7, decimal_places=2)
+    location = models.CharField(max_length=100)
+    num_rooms = models.PositiveSmallIntegerField()
+    status = models.CharField(max_length=10, choices=[('Available', 'Available'), ('Sold', 'Sold')])
+    sold_date = models.DateField(null=True, blank=True)
+    property_type = models.CharField(max_length=20, choices=[('Loft', 'Loft'), ('Flat', 'Flat'), ('Villa', 'Villa'), ...])
+    num_interests = models.PositiveIntegerField(default=0)
+    image = models.ImageField(
+        upload_to='images/', default='../default_post_rgq6aq', blank=True
+    )
+    images = models.ManyToManyField(Image)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f'{self.id} {self.title}'
---
+++ properties/serializers.py
@@ -0,0 +1,48 @@
+from rest_framework import serializers
+from properties.models import Property
+
+
+class PropertySerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+    is_owner = serializers.SerializerMethodField()
+    profile_id = serializers.ReadOnlyField(source='owner.profile.id')
+    profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+
+
+    def validate_image(self, value):
+        if value.size > 2 * 1024 * 1024:
+            raise serializers.ValidationError('Image size larger than 2MB!')
+        if value.image.height > 4096:
+            raise serializers.ValidationError(
+                'Image height larger than 4096px!'
+            )
+        if value.image.width > 4096:
+            raise serializers.ValidationError(
+                'Image width larger than 4096px!'
+            )
+        return value
+
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
+
+    class Meta:
+        model = Property
+        fields = [
+            'id', 'owner', 'is_owner', 'profile_id', 'profile_image',
+            'created_at', 'updated_at', 'title', 'description',
+            'price', 'size', 'location', 'num_rooms', 'status',
+            'sold_date', 'property_type', 'num_interests',
+            'image' 
+        ]
+
+
+
+
+    
+
+    
+
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
