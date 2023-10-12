c0fd7a27733c40c4b598a139c15c9a5d390599ab
danio86 <d.harder@gmx.at>
2023-10-12 06:27:21 +0000

    add imagefilter to properties

--- properties/models.py
+++ properties/models.py
@@ -2,15 +2,33 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class Image(models.Model):
-    property = models.ForeignKey(Property, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='property_images/')
+# class Image(models.Model):
+#     property = models.ForeignKey(Property, on_delete=models.CASCADE)
+#     image = models.ImageField(upload_to='property_images/')
 
 class Property(models.Model):
     """
     Property model, related to 'owner', i.e. a User instance.
     Default image set so that we can always reference image.url.
     """
+
+    image_filter_choices = [
+        ('_1977', '1977'),
+        ('brannan', 'Brannan'),
+        ('earlybird', 'Earlybird'),
+        ('hudson', 'Hudson'),
+        ('inkwell', 'Inkwell'),
+        ('lofi', 'Lo-Fi'),
+        ('kelvin', 'Kelvin'),
+        ('normal', 'Normal'),
+        ('nashville', 'Nashville'),
+        ('rise', 'Rise'),
+        ('toaster', 'Toaster'),
+        ('valencia', 'Valencia'),
+        ('walden', 'Walden'),
+        ('xpro2', 'X-pro II')
+    ]
+
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -22,12 +40,23 @@ class Property(models.Model):
     num_rooms = models.PositiveSmallIntegerField()
     status = models.CharField(max_length=10, choices=[('Available', 'Available'), ('Sold', 'Sold')])
     sold_date = models.DateField(null=True, blank=True)
-    property_type = models.CharField(max_length=20, choices=[('Loft', 'Loft'), ('Flat', 'Flat'), ('Villa', 'Villa'), ...])
+    property_type = models.CharField(
+            max_length=20,
+            choices=[
+                ('Loft', 'Loft'),
+                ('Flat', 'Flat'),
+                ('Villa', 'Villa'),
+                # Add more choices as needed
+            ],
+        )
     num_interests = models.PositiveIntegerField(default=0)
     image = models.ImageField(
         upload_to='images/', default='../default_post_rgq6aq', blank=True
     )
-    images = models.ManyToManyField(Image)
+    image_filter = models.CharField(
+    max_length=32, choices=image_filter_choices, default='normal'
+    )
+    # images = models.ManyToManyField(Image)
 
     class Meta:
         ordering = ['-created_at']
