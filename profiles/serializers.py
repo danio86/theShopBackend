584e2b5cecb1ab201cbbcb13f8adb43dc90b0cd3
danio86 <d.harder@gmx.at>
2023-10-14 18:12:36 +0000

    add fild filter to profile list and detail view

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -4,7 +4,11 @@ from .models import Profile
 class ProfileSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
-    sold_id = serializers.SerializerMethodField()
+    # sold_id = serializers.SerializerMethodField()
+    properties_count = serializers.ReadOnlyField()
+    prospective_buyer_count = serializers.ReadOnlyField()
+    sold_count = serializers.ReadOnlyField()
+    # following_count = serializers.ReadOnlyField()
 
     def get_is_owner(self, obj):
         request = self.context['request']
@@ -14,5 +18,7 @@ class ProfileSerializer(serializers.ModelSerializer):
         model = Profile
         fields = [
             'id', 'owner', 'created_at', 'updated_at', 'name',
-            'content', 'image', 'is_owner'
+            'content', 'image', 'is_owner', 'properties_count',
+            'prospective_buyer_count', 'sold_count'
+            # 'following_count'
         ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,7 +1,8 @@
-from rest_framework import generics
+from rest_framework import generics, filters
 from the_shop_api.permissions import IsOwnerOrReadOnly
 from .models import Profile
 from .serializers import ProfileSerializer
+from django.db.models import Count
 
 
 class ProfileList(generics.ListAPIView):
@@ -9,8 +10,26 @@ class ProfileList(generics.ListAPIView):
     List all profiles.
     No create view as profile creation is handled by django signals.
     """
-    queryset = Profile.objects.all()
+    queryset = Profile.objects.annotate(
+        sold_count=Count('owner__property', distinct=True),
+        properties_count=Count('owner__property', distinct=True),
+        prospective_buyer_count=Count('owner__prospectivebuyer', distinct=True),
+        # following_count=Count('owner__following', distinct=True)
+    ).order_by('-created_at')
+    
     serializer_class = ProfileSerializer
+    filter_backends = [
+        filters.OrderingFilter
+    ]
+    ordering_fields = [
+        'sold_count',
+        'owner__sold__created_at',
+        'properties_count',
+        'prospective_buyer_count',
+        # 'following_count',
+        # 'owner__following__created_at',
+        'owner__prospective_buyer__created_at',
+    ]
 
 
 class ProfileDetail(generics.RetrieveUpdateAPIView):
@@ -18,5 +37,10 @@ class ProfileDetail(generics.RetrieveUpdateAPIView):
     Retrieve or update a profile if you're the owner.
     """
     permission_classes = [IsOwnerOrReadOnly]
-    queryset = Profile.objects.all()
+    queryset = Profile.objects.annotate(
+            sold_count=Count('owner__property', distinct=True),
+            properties_count=Count('owner__property', distinct=True),
+            prospective_buyer_count=Count('owner__prospectivebuyer', distinct=True),
+            # following_count=Count('owner__following', distinct=True)
+        ).order_by('-created_at')
     serializer_class = ProfileSerializer
