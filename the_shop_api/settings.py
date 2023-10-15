736e6283dee0296016224190e9ee5b7e3617ca56
danio86 <d.harder@gmx.at>
2023-10-15 13:07:38 +0000

    changed allowed hosts in settings.py

--- the_shop_api/settings.py
+++ the_shop_api/settings.py
@@ -77,11 +77,11 @@ DEBUG = 'DEV' in os.environ
 
 
 # ALLOWED_HOSTS = ['yourdomain.com', '8000-danio86-theshopbackend-0qerx522l2z.ws-eu105.gitpod.io']
-# ALLOWED_HOSTS = ['localhost', 'the-shop2-0.herokuapp.com']
-ALLOWED_HOSTS = [
-    'localhost', os.environ.get('ALLOWED_HOST'),
-    '8000-danio86-theshopbackend-0qerx522l2z.ws-eu105.gitpod.io',
-]
+ALLOWED_HOSTS = ['localhost', 'the-shop2-0-d8de1f67b769.herokuapp.com']
+# ALLOWED_HOSTS = [
+#     'localhost', os.environ.get('ALLOWED_HOST'),
+#     '8000-danio86-theshopbackend-0qerx522l2z.ws-eu105.gitpod.io',
+# ]
 
 
 # Application definition
